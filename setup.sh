5514b5b7442b097ecec77f13faa997a8ace50000
razorc4 <49373580+razorc4@users.noreply.github.com>
2019-04-07 16:26:36 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,10 @@ sudo mysql_secure_installation
 ## Copy 'Application' from repo.
 ## Replace later with actual steps. 
 
+## Change ownership/groups of files to www-data
+
+## Ensure *.cgi files have eXecute permissions by owner.
+
 ## Enable CGI scripts. 
 sudo a2enmod cgid 
 sudo systemctl restart apache2 
@@ -14,3 +18,19 @@ vi /etc/apache2/conf-available/cgi-enabled.conf
     Options +ExecCGI
     AddHandler cgi-script .cgi .pl
 </Directory>
+
+a2enconf cgi-enabled
+sudo systemctl restart apache2 
+
+## Setup CPAN modules for perl use. 
+sudo apt-get install make
+sudo cpan
+sudo cpan Switch
+sudo cpan Appache::Session , Time::Local, URI::Escape,  CGI qw/:standard *table start_ul/;
+
+## Install phpamyadmin
+sudo apt-get install phpmyadmin
+sudo phpenmod mbstring
+sudo systemctl restart apache2 
+
+
