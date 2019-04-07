0f43c72e0b9b2695885d9ee4e40529f457063f91
razorc4 <49373580+razorc4@users.noreply.github.com>
2019-04-07 15:41:47 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,16 @@
 sudo apt-get install apache2
 sudo apt-get install mariadb-server
 sudo mysql_secure_installation
+## Copy 'Application' from repo.
+## Replace later with actual steps. 
+
+## Enable CGI scripts. 
+sudo a2enmod cgid 
+sudo systemctl restart apache2 
+
+## Edit /etc/apache2/conf-available/cgi-enabled.conf
+vi /etc/apache2/conf-available/cgi-enabled.conf
+<Directory "/var/www/html/">
+    Options +ExecCGI
+    AddHandler cgi-script .cgi .pl
+</Directory>
